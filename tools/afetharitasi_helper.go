9deccd84a440df21738d561d694793360e489d5a
Yusuf Çınar Özmen <yusuf@Macbook-Air.local>
2023-02-12 00:49:02 +0300

    Removed panic

--- tools/afetharitasi_helper.go
+++ tools/afetharitasi_helper.go
@@ -51,12 +51,12 @@ func GetSingleLocation(ctx context.Context, locationID int, cache sources.Cache)
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
 	})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	singleData := &SingleResponse{}
 	if err := json.Unmarshal(resp, singleData); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	cache.Set(fmt.Sprintf("single_location_%d", locationID), singleData, 0)
